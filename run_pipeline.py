2ff1c9cf49b24ef4cf67984f4d672880d3fac833
Amit raj <amit.rock9889@gmail.com>
2025-04-13 16:39:14 +0530

    Normal commit

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+
+    # Run the pipeline
+
+    train_pipeline(data_path="C:/Users/amitr/Desktop/MLOPs/data/olist_customers_dataset.csv")
